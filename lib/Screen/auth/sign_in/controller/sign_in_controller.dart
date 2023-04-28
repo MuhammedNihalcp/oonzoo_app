847bc6c334bcc3e05d321a8c75a2651751616481
MuhammedNihalcp <nihalriju915@gmail.com>
2023-04-28 11:55:57 +0530

    remove unused package

--- lib/Screen/auth/sign_in/controller/sign_in_controller.dart
+++ lib/Screen/auth/sign_in/controller/sign_in_controller.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:get/get.dart';
 import '../../../../core/colors.dart';
 import '../../../home/view/home_view.dart';
